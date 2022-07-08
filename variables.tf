b9db3932d2bb483fb588bd03762d6f608ab3acb8
Richard <richwats@cisco.com>
2022-07-08 15:44:25 +1000

    vars

--- variables.tf
+++ variables.tf
@@ -8,15 +8,7 @@ variable "aci_password" {
 variable "aci_url" {
 }
 
-# ### Existing ACI Virtual Machine Manager (VMM) Domain ###
-# variable "vmm_name" {
-#   type = string
-# }
-#
-# ### Existing Physical Domain for Static EPGs ###
-# variable "phys_name" {
-#   type = string
-# }
-
 ### Tennant Input Variable Object ###
-variable "tenants" {}
+variable "common_tenant" {}
+
+variable "demo_tenant" {}
